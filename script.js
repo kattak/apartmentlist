8fc040ff9522d42051fb4368eeaefd5636f155b5
kattak <macbookpro@Macbooks-MacBook-Pro-3.local>
2017-06-02 10:23:16 -0700

    Start with modified payload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+var payload = {
+    "members": [
+        {
+            "email": "jill@mycompany.com",
+            "name": "Jill",
+            "team": "engineering"
+        },
+        {
+            "email": "rohit@mycompany.com",
+            "name": "Rohit",
+            "team": "finance"
+        },
+        {
+            "email": "maria@mycompany.com",
+            "name": "Maria",
+            "team": "operations"
+        },
+        {
+            "email": "sam@mycompany.com",
+            "name": "Sam",
+            "team": "operations"
+        }
+    ],
+    "restaurant": {
+        "logo": "http://example.com/images/pizza.png",
+        "name": "Pizza R Us",
+        "yelp_link": "https://yelp.com/pizza-r-us"
+    }
+}

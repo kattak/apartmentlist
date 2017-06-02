07ad9c65f86729549570782f93f84aeea6321926
kattak <macbookpro@Macbooks-MacBook-Pro-3.local>
2017-06-02 10:50:10 -0700

    Add tests for restaurant, members, and member

--- test.js
+++ test.js
@@ -13,11 +13,41 @@ var assert = chai.assert;
 //   it('should not add a class which already exists in element');
 // });
 
-describe('member', function() {
-  it('should have a name', function() {
-    var member = payload["members"][0];
+describe('restaurant', function() {
+  var restaurant = payload["restaurant"];
 
-  assert.equal(member["name"], 'Jill');
+  it('should have a logo', function() {
+    assert.equal(restaurant["logo"], "http://example.com/images/pizza.png");
+  });
+
+  it('should have a name', function() {
+    assert.equal(restaurant["name"], "Pizza R Us");
+  });
+
+  it('should have a Yelp link', function() {
+    assert.equal(restaurant["yelp_link"], "https://yelp.com/pizza-r-us");
+  });
+});
+
+describe('members', function() {
+  var members = payload["members"];
+
+  it('should have four members', function() {
+    assert.equal(members.length, 4);
+  });
+});
+
+describe('member', function() {
+  var member = payload["members"][0];
+
+  it('should have a name', function() {
+    assert.equal(member["name"], 'Jill');
+  });
+  it('should have an email', function() {
+    assert.equal(member["email"], 'jill@mycompany.com');
+  });
+  it('should have an team', function() {
+    assert.equal(member["team"], 'engineering');
   });
 });
 
